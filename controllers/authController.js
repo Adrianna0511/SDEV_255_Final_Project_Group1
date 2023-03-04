4b51dfa3c94096f0048093639f227a7d5042b96e
mbrky6 <mberkey6@ivytech.edu>
2023-03-04 17:39:45 -0500

    Made a distinction between student and teacher accounts.

--- controllers/authController.js
+++ controllers/authController.js
@@ -52,10 +52,10 @@ module.exports.login_get = (req, res) => {
 }
 
 module.exports.signup_post = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, teacher } = req.body;
 
   try {
-    const user = await User.create({ email, password });
+    const user = await User.create({ email, password, teacher });
     const token = createToken(user._id);
     res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000});
     res.status(201).json({user: user._id});
@@ -68,7 +68,7 @@ module.exports.signup_post = async (req, res) => {
 }
 
 module.exports.login_post = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, teacher } = req.body;
 
   try {
     const user = await User.login(email, password);
